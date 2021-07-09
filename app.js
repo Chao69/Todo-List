43b39f5a326059a2aa31f4287fec839a0b01f61e
chao69 <star010631@gmail.com>
2021-07-09 19:29:13 +0800

    feat:add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const app = express()
 // connect to mondoDB/solve the connect DeprecationWarning
@@ -14,9 +15,14 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+// define express-handlebars engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
 // define 根目錄 static view
 app.get('/', (req, res) => {
-  res.send(`<h1>hello world!</h1>`)
+  res.render('index')
 })
 // http://localhost:3000 connect succeed tips
 app.listen(3000, () => {
