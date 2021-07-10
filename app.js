704dd10b569afb13c83f20c7437eb76e1ccd933a
chao69 <star010631@gmail.com>
2021-07-10 17:12:21 +0800

    feat:modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
 
 const app = express()
 // connect to mondoDB/solve the connect DeprecationWarning
@@ -17,12 +18,16 @@ db.once('open', () => {
 })
 
 // define express-handlebars engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // define 根目錄 static view
 app.get('/', (req, res) => {
-  res.render('index')
+  // 取得所有Todo資料
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.error(error))
 })
 // http://localhost:3000 connect succeed tips
 app.listen(3000, () => {
