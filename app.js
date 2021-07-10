13c2eb45aa5d178b088028440d5b4105bcc4e191
chao69 <star010631@gmail.com>
2021-07-10 20:17:32 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -59,6 +59,26 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// todo edit url
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
 // http://localhost:3000 connect succeed tips
 app.listen(3000, () => {
   console.log('App is runnung on http://localhost:3000.')
