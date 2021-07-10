d972462fcf2c9b81b7565d67b777a9c6a4c36840
chao69 <star010631@gmail.com>
2021-07-10 18:03:06 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Todo = require('./models/todo')
+const bodyParser = require('body-parser')
 
 const app = express()
 // connect to mondoDB/solve the connect DeprecationWarning
@@ -12,6 +13,7 @@ const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
 })
+
 // db connect succeed tips
 db.once('open', () => {
   console.log('mongodb connected!')
@@ -21,6 +23,9 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// use body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // define 根目錄 static view
 app.get('/', (req, res) => {
   // 取得所有Todo資料
@@ -29,6 +34,22 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
+
+// create new todo
+app.get('/todos/new', (req, res) => {
+  // link to create page
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  // create new todo to database & home page
+  const name = req.body.name
+
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(erro => console.log(error))
+})
+
 // http://localhost:3000 connect succeed tips
 app.listen(3000, () => {
   console.log('App is runnung on http://localhost:3000.')
